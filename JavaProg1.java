4f960e32d4a0cac4a08adb82b8b25d5ae877d75a
tajwerosmani <76664604+tajwerosmani@users.noreply.github.com>
2020-12-27 15:57:14 -0500

    Add files via upload
    First-ever Java program uploaded to GitHub

---
+++ JavaProg1.java
@@ -0,0 +1,11 @@
+
+public class JavaProg1 {
+
+	public static void main(String[] args) {
+		// This is acomment no action by java will work on this line
+		System.out.println("This is display in the console out put");
+		System.out.println("Thi line will also display");
+
+	}
+
+}
